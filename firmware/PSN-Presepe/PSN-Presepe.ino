8f83d3d12f018a31d9e11462d3eac2b61bbdf9d8
zanac <21194919+zanac@users.noreply.github.com>
2026-09-24 20:49:12 +0200

    Add firmware revision 001 to OLED splash

--- firmware/PSN-Presepe/PSN-Presepe.ino
+++ firmware/PSN-Presepe/PSN-Presepe.ino
@@ -445,7 +445,7 @@ bool inizializzaOled() {
   display.setTextSize(1);
   // Startup splash: PSN-Presepe! by Vanni
   display.setCursor(27,18); display.print(F("PSN-Presepe!"));
-  display.setCursor(39,36); display.print(F("by Vanni"));
+  display.setCursor(30,36); display.print(F("by Vanni 001"));
   display.display();
   delay(2000);
   return true;
